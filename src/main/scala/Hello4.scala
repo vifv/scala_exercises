091ed32cadf6ab0c076650b83cd39320796a8779
vifv <84492287+vifv@users.noreply.github.com>
2021-05-24 16:26:38 +0500

    [see #4] Решения упражнений 4 главы

--- src/main/scala/Hello4.scala
+++ src/main/scala/Hello4.scala
@@ -1,5 +1,7 @@
 import java.util.Scanner
 import scala.collection.mutable
+import scala.io.Source
+import scala.util.Sorting
 
 object Hello4 extends App {
   print("Решение упражнений 4 главы")
@@ -8,36 +10,32 @@ object Hello4 extends App {
 // Задача №1
 object Ch4Ex1 extends App {
   val shop = mutable.Map("PC" -> 1000, "Phone" -> 500, "Juice" -> 10)
-  val betterShop = shop.view.mapValues(price => price*0.9)
-  for((k ,v) <- betterShop) println(k ,v)
+  val betterShop = shop.view.mapValues(price => price * 0.9)
+  for ((k, v) <- betterShop) println(k, v)
 }
 
 // Задача №2
 object Ch4Ex2 extends App {
-  val mapa = mutable.Map[String, Int]("qwertyasdfgh" -> 1)
-  val in = new Scanner(new java.io.File("/Users/vladislav/scala/scala_exercises/src/main/resources/myfile.txt"))
-    while (in.hasNext()) {
-      val str = in.next().replace("." , "").replace(",", "")
-        .replace("-", "").replace("?", "")
-        .replace("!", "").split(" ") //Здесь я очищаю текст от пунктуационных знаков и разделяю на массив слов
-      for (s <- str) { // Здесь по моей задумке цикл должен проходить по каждому слову в своем массиве и сравнивать с картой,
-                       // если слово совпадает с ключом - то закидываем в мапу этот же ключ со значением+1
-                       // если нет, то добавляем ключ со значением 1
-        for ((k, v) <- mapa)
-          if (s.equals(k))
-            mapa +=(k -> (v+1))
-          else
-            mapa +=(s -> 1)
-      }
-    }
-  mapa -=("qwertyasdfgh")
-  for ((k,v) <- mapa) println (k,v)
+  val source = Source.fromFile("/Users/vladislav/scala/scala_exercises/src/main/resources/myfile.txt")
+  val wordsArray = source.mkString
+    .replaceAll("\\p{Punct}", " ")
+    .replaceAll("^", " ")
+    .replaceAll("$", " ")
+    .replaceAll("\\s+"," ")
+    .split(" ")
+  source.close()
+  val lowCaseWordsArr = wordsArray.map(_.toLowerCase)
+  val wordsMap = lowCaseWordsArr.groupBy(identity)
+  var wordsMapRes = mutable.Map[String, Int]()
+    for ((k,v) <- wordsMap) wordsMapRes +=(k -> v.length)
+  for ((k,v) <- wordsMapRes) println(k,v)
 }
+
 
 
 // Задача №6
 object Ch4Ex6 extends App {
-  val calend = mutable.LinkedHashMap[String, Int](
+  val calendar = mutable.LinkedHashMap[String, Int](
     "Понедельник" -> java.util.Calendar.MONDAY,
     "Вторник" -> java.util.Calendar.TUESDAY,
     "Среда" -> java.util.Calendar.WEDNESDAY,
@@ -46,5 +44,38 @@ object Ch4Ex6 extends App {
     "Суббота" -> java.util.Calendar.SATURDAY,
     "Воскресенье" -> java.util.Calendar.SUNDAY
   )
-  for ((k,v) <- calend) println(k,v)
+  for ((k, v) <- calendar) println(k, v)
+}
+
+// Задача №8
+object Ch4Ex8 extends App {
+  def minmax(values: Array[Int]): (Int, Int) = {
+    Sorting.quickSort(values)
+    values(0) -> values.reverse(0)
+  }
+  val arr = Array(1,6,9,3,56,12,34,-2,0,22)
+  print(minmax(arr))
+}
+
+// Задача №9
+ object Ch4Ex9 extends App {
+  def lteqgt(values: Array[Int], v: Int): (Int, Int, Int) = {
+    var countMin = 0
+    var countMax = 0
+    var countEq = 0
+    for (f <- values) {
+      if (f < v) countMin +=1
+      else if (f > v) countMax +=1
+      else countEq +=1
+    }
+    Tuple3(countMin, countEq, countMax)
+  }
+    val values = Array(1,2,3,4,5,6,7,8,9,10)
+    print(lteqgt(values, 3))
+}
+
+// Задача №10
+// Получаем Вектор кортежей
+object Ch10Ex10 extends App {
+  print("Hello".zip("World"))
 }
